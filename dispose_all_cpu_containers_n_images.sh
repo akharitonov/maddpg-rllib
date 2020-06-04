50bdb2768dd66cd8df679e970be7cfb5dc8df078
akharitonov <32765888+akharitonov@users.noreply.github.com>
2020-06-04 22:09:23 +0200

    Fixed: disposal convenience script - remove volumes

--- dispose_all_cpu_containers_n_images.sh
+++ dispose_all_cpu_containers_n_images.sh
@@ -8,6 +8,8 @@ max=5
 for i in $(seq 0 $max)
 do
     docker container rm -v ${PFX}maddpg-rllib_$i
+    docker volume rm ${PFX}maddpg-rllib-vtmp_$i
+    docker volume rm ${PFX}maddpg-rllib-vres_$i
 done
 
 
